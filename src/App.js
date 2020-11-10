1674a10a8f2d4c5a8a7a8c1bbe25c9f377d85af8
carlpadilla <carlpadilla@protonmail.com>
2020-11-10 13:18:31 -0500

    added play function

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ function App() {
   //state
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className='App'>
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
