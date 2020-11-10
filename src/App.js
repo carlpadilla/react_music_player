35a6b8efff432b10c35ce0d3cec3b2d8b83eb95d
carlpadilla <carlpadilla@protonmail.com>
2020-11-10 12:42:03 -0500

    added  utils data file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
 
 //components
 import Player from './components/Player';
 import Song from './components/Song';
 
+//util
+import data from './utils';
+
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className='App'>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
